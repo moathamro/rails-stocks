7333c3decb697ffa4b19ce9a69ec13ce0977af66
dutsy <37940638+dutsy@users.noreply.github.com>
2020-11-09 01:16:13 +0200

    stock list page

---
+++ myapp/static/js/search.js
@@ -0,0 +1,54 @@
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+
+function openCheckbox() {
+    document.getElementById("myCheckbox").style.display = "block";
+}
+
+function closeCheckbox() {
+    document.getElementById("myCheckbox").style.display = "none";
+}
+
+$('#main-Form').submit(function(e) {
+    if(cheackdata()){
+        e.preventDefault();
+    }
+});
+
+
+function cheackdata(event) {
+
+    var searched = $("[name='stock_name']").val();
+
+    if ($('#symbol-checkbox').is(":checked")) {
+        if (/[^a-zA-Z]/.test(searched)) {
+            alert("please enter a letters");
+            return true
+        }
+    } else if ($('#name-checkbox').is(":checked")) {
+        if (/[^a-zA-Z]/.test(word)) {
+            alert("please enter a letters")
+            return true
+        }
+    } else if ($('#price-checkbox').is(":checked")) {
+        if (!$.isNumeric(searched)) {
+            alert("please enter numbers")
+            return true
+        }
+    } else if ($('#change-checkbox').is(":checked")) {
+
+    }
+}
+
+// $( "tr" ).hover(
+//   function() {
+//     $( this ).append( $( "<span> ***</span>" ) );
+//   }, function() {
+//     $( this ).find( "span" ).last().remove();
+//   }
+// );
--- myapp/urls.py
+++ myapp/urls.py
@@ -3,10 +3,12 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-	path('', views.index, name='index'),
-	path('stock/<str:symbol>/', views.single_stock, name='single_stock'),
-	path('historic/<str:symbol>/', views.single_stock_historic, name='single_stock_historic'),
-	path('accounts/login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-	path('accounts/logout/', views.logout_view, name='logout'),
-	path('accounts/register/', views.register, name='register'),
+    path('', views.index, name='index'),
+    path('stock/<str:symbol>/', views.single_stock, name='single_stock'),
+    path('historic/<str:symbol>/', views.single_stock_historic, name='single_stock_historic'),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('accounts/logout/', views.logout_view, name='logout'),
+    path('accounts/register/', views.register, name='register'),
+    path('search/', views.search, name='search'),
+    path('viewall', views.view_all, name='view_all'),
 ]
