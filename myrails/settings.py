c5568c651b2faaa3f51de91d39767a60b4b69d11
khalid awwad (outlook) <khalid.awwad3@outlook.com>
2020-11-23 19:48:54 +0000

    Updated settings.py

--- myrails/settings.py
+++ myrails/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'myrails.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'mnt/db.sqlite3'),
+        'NAME': '/mnt/db.sqlite3',
     }
 }
 
