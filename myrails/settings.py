19c478efe7063389dcf7e1f2ff6e94f38b0d9cc3
moathamro <amromoath1@gmail.com>
2020-11-26 18:03:36 +0200

    edit settings

--- myrails/settings.py
+++ myrails/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
+
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -124,7 +125,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static').replace('\\','/')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'myapp/static/img')
 
